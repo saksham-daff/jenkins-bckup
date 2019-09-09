aabec4ab9bf26ec4b96ee75457e49ea66cd54a1c
Saksham <saksham@daffodil.com>
2019-09-09 19:05:36 +0000

    Added gitIgnore File with script

---
+++ create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+git add -- *.xml jobs/*/*.xml userContent/*
+
+# only add user configurations if they exist
+if [ -d users ]; then
+    user_configs=`ls users/*/config.xml`
+
+    if [ -n "$user_configs" ]; then
+        git add $user_configs
+    fi
+fi
+
+# mark as deleted anything that's been, well, deleted
+to_remove=`git status | grep "deleted" | awk '{print $3}'`
+
+if [ -n "$to_remove" ]; then
+    git rm --ignore-unmatch $to_remove
+fi
+
+git commit -m "Automated Jenkins commit"
+
+git push -q -u origin master
+
+echo "Done"
